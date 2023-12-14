1f3d3ffd5b2f4a90587071b8a935ae013b2d10fc
Joe Pickert <joe@joepickert.com>
2023-12-13 20:54:26 -0600

    Housekeeping

--- Day 11/part1and2.rb
+++ Day 11/part1and2.rb
@@ -3,18 +3,17 @@ universe = File.read('input.txt').split("\n")
 galaxies = []
 expandy = []
 expandx = []
-factors = [2, 1000000]
 
-# original universe galaxy positions + y posiitions to be expanded
+# original universe galaxy positions + y positions to be expanded
 universe.each.with_index do |l, row|
   positions = l.enum_for(:scan, /#/).map { Regexp.last_match.begin(0) }
   galaxies += positions.map{|col| [row, col]}
-  expandy.push row if positions.empty?
+  expandy.push(row) if positions.empty?
 end
 
 # x positions to be expanded
-(0..universe[0].length-1).each do |x|
-  expandx.push(x) if universe.map {|r| r[x]}.uniq == ['.']
+(0..universe[0].length-1).each do |col|
+  expandx.push(col) if universe.map {|x| x[col]}.uniq == ['.']
 end
 
 # perform expansion of universe by modifing galaxy positions accordingly
@@ -26,14 +25,10 @@ def doexpansion(galaxies, expandy, expandx, factor)
   end
 end
 
-# chessboard distance calculation between two galaxies
-def chessboard_distance(g1, g2)
-  (g1[0] - g2[0]).abs + (g1[1] - g2[1]).abs
-end
-
-factors.map do |f|
+[2, 1000000].map do |f|
   r = doexpansion(galaxies, expandy, expandx, f).combination(2).map do |g1, g2|
-    chessboard_distance(g1, g2)
+    # chessboard distance calculation between two galaxies
+    (g1[0] - g2[0]).abs + (g1[1] - g2[1]).abs
   end
   p r.sum
 end
